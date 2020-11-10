11b0128db1df871bd6f98db73eddde6dba0d7148
Pavol Noha <pavol.noha@gmail.com>
2020-11-10 21:17:21 +0100

    upgrade bundle to figwheel-main

--- bundle/project.clj
+++ bundle/project.clj
@@ -6,6 +6,7 @@
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [org.clojure/clojurescript "1.10.597"]
                  [org.clojure/tools.logging "0.4.1"]
+                 [org.clojure/core.async "1.3.610"]
                  [metosin/reitit "0.3.10"]
                  [http-kit "2.4.0"]
                  [enlive "1.1.6"]
@@ -19,69 +20,39 @@
                  [cljs-ajax "0.8.1"]
                  [day8.re-frame/http-fx "0.2.1"]
                  [com.fasterxml.jackson.core/jackson-core "2.10.0"]
-                 [com.fasterxml.jackson.core/jackson-databind "2.10.0"]]
+                 [com.fasterxml.jackson.core/jackson-databind "2.10.0"]
+                 [org.eclipse.jetty/jetty-server "9.4.28.v20200408"]
+                 [org.eclipse.jetty.websocket/websocket-servlet "9.4.28.v20200408"]
+                 [org.eclipse.jetty.websocket/websocket-server "9.4.28.v20200408"]]
   :min-lein-version "2.8.2"
-  :plugins [[lein-cljsbuild "1.1.7"]]
   :source-paths ["src/clj" "src/cljs" "src/cljc"]
   :test-paths ["test/clj" "test/cljc"]
-  :clean-targets ^{:protect false} ["resources/public/js/out"
-                                    "resources/public/js/webapp.js"
-                                    :target-path]
-  :uberjar-name "webapp.jar"
+  :resource-paths ["resources"]
   :main webapp.server
-  :repl-options {:init-ns repl.user}
-  :cljsbuild {:builds
-              [{:id           "dev"
-                :source-paths ["src/cljs" "src/cljc"]
 
-                ;; The presence of a :figwheel configuration here
-                ;; will cause figwheel to inject the figwheel client
-                ;; into your build
-                :figwheel     true
-                :compiler     {:main                 webapp.app
-                               :asset-path           "/js/out"
-                               :output-to            "resources/public/js/webapp.js"
-                               :output-dir           "resources/public/js/out"
-                               :infer-externs        true
-                               :parallel-build       true
-                               :source-map-timestamp true
-                               ;; To console.log CLJS data-structures make sure you enable devtools in Chrome
-                               ;; https://github.com/binaryage/cljs-devtools
-                               :preloads             [devtools.preload]}}
+  :plugins [[lein-shell "0.5.0"]]
 
-               ;; This next build is a compressed minified build for
-               ;; production. You can build this with:
-               ;; lein cljsbuild once min
-               {:id           "min"
-                :source-paths ["src/cljs" "src/cljc"]
-                :compiler     {:main                 webapp.app
-                               :output-to            "resources/public/js/webapp.js"
-                               :output-dir           "target"
-                               :parallel-build       true
-                               :infer-externs        true
-                               :source-map-timestamp true
-                               :optimizations        :advanced
-                               :closure-defines      {"goog.DEBUG" false}
-                               :pretty-print         false
-                               :pseudo-names         false}}]}
-  :figwheel {:http-server-root "public-assets"
-             :css-dirs         ["resources/public/css"]
-             :ring-handler     repl.user/http-handler
-             :server-logfile   "log/figwheel.log"}
+  :aliases {"fig"      ["trampoline" "run" "-m" "figwheel.main"]
+            "fig:dev"  ["trampoline" "run" "-m" "figwheel.main" "-b" "dev" "-r"]
+            "fig:min"  ["run" "-m" "figwheel.main" "-O" "advanced" "-co" "min.cljs.edn" "-c"]
+            "clean!"   ["do"
+                        ["shell" "rm" "-rf" "target"]
+                        ["shell" "rm" "-rf" "resources/public/js/out"]
+                        ["shell" "rm" "resources/public/js/webapp.js"]]
+            "release!" ["do"
+                        ["fig:min"]
+                        ["uberjar"]]}
 
-  ;; Setting up nREPL for Figwheel and ClojureScript dev
-  ;; Please see:
-  ;; https://github.com/bhauman/lein-figwheel/wiki/Using-the-Figwheel-REPL-within-NRepl
-  :profiles {:dev     {:dependencies [[figwheel "0.5.17"]
-                                      [figwheel-sidecar "0.5.17"]
-                                      [binaryage/devtools "0.9.10"]
+  :profiles {:dev     {:dependencies [[com.bhauman/figwheel-main "0.2.12"]
+                                      [com.bhauman/rebel-readline-cljs "0.1.4"]
                                       [cider/piggieback "0.4.1"]]
-                       :plugins      [[lein-figwheel "0.5.17"]
-                                      [lein-doo "0.1.6"]]
                        :source-paths ["dev"]
-                       :repl-options {:nrepl-middleware [cider.piggieback/wrap-cljs-repl]}}
+                       :repl-options {:nrepl-middleware [cider.piggieback/wrap-cljs-repl]
+                                      :init-ns          repl.user
+                                      :port             56600}}
              :prod    {:source-paths ^:replace ["src/clj" "src/cljc"]
-                       :prep-tasks   ["javac" "compile" ["cljsbuild" "once" "min"]]
+                       :uberjar-name "webapp.jar"
+                       :prep-tasks   ["javac" "compile"]
                        :omit-source  true
                        :aot          :all}
              :uberjar [:prod]})
